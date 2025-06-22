d8cee8f86ff70d12b61692d336e8f3b82e5b922c
bhuvana-v06 <bhuvaneswari7899@gmail.com>
2025-06-22 18:00:40 +0530

    This program represent MultiLevel Inheritance

---
+++ C2TC/src/Day5_MLinheritance2/Maruti800.java
@@ -0,0 +1,12 @@
+package Day5_MLinheritance2;
+
+public class Maruti800 extends Maruti{
+	public Maruti800() {
+		System.out.println("Maruti Model: 800");
+	}
+
+	public void speed() {
+		System.out.println("Max: 80Kmph");
+	}
+
+}
