b117cb3b190a0aaab4553586fab7a5463911d0fc
bhuvana-v06 <bhuvaneswari7899@gmail.com>
2025-06-22 21:19:17 +0530

    This program contians overriding

---
+++ C2TC/src/Day7_Overriding/HDFC.java
@@ -0,0 +1,10 @@
+package Day7_Overriding;
+
+    //subclass
+	public class HDFC extends RBI {
+		@Override
+		public float getRateOfInterest() {
+			return 6.8f;
+		}
+
+}
---
+++ C2TC/src/Day7_Overriding/RBI.java
@@ -0,0 +1,10 @@
+package Day7_Overriding;
+
+public class RBI {
+	//Superclass
+	
+		public float getRateOfInterest() {
+			return 6.7f;
+		}
+
+}
