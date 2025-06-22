e23631cb02710e24b4196acb78433896e12bc23a
bhuvana-v06 <bhuvaneswari7899@gmail.com>
2025-06-22 17:23:41 +0530

    This programs represent single inheritance.

---
+++ C2TC/src/Day5_SLinheritance/SingleInheritanceDemo.java
@@ -0,0 +1,10 @@
+package Day5_SLinheritance;
+
+public class SingleInheritanceDemo {
+
+	public static void main(String[] args) {
+		
+		Student student = new Student("Gayatri", "8080808080", "Bangalore", 9888878787L, 10, "City College");
+		System.out.println(student);
+	}
+}
