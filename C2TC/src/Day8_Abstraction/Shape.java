1496da9e563d4ec26189fb9d61d4e3c0666fef39
bhuvana-v06 <bhuvaneswari7899@gmail.com>
2025-06-22 20:34:07 +0530

    This program represent abstraction.

---
+++ C2TC/src/Day8_Abstraction/Shape.java
@@ -0,0 +1,16 @@
+package Day8_Abstraction;
+
+public abstract class Shape {
+
+	     protected float area;
+	     
+	     //abstract method 
+		 abstract void calArea();
+		 
+		 //concrete method
+		 void show()
+		 {
+			 System.out.println("Area of shape is "+area);
+		 }
+
+}
